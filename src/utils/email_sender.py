60e59a6c10b4ebd32886f80197957c6a3406ef2f
unknown <sayhellotodevops@gmail.com>
2026-05-24 11:26:01 +0200

    .

--- src/utils/email_sender.py
+++ src/utils/email_sender.py
@@ -12,16 +12,16 @@ def send_interview_email(to_email, candidate_name, vacancy_title, company_name="
     
     subject = f"Interview Invitation - {vacancy_title}"
     body = f"""
-        Dear {candidate_name},
+    Dear {candidate_name},
 
-        Thank you for applying for the {vacancy_title} position at {company_name}.
+    Thank you for applying for the {vacancy_title} position at {company_name}.
 
-        We are pleased to inform you that your application has been shortlisted. We would like to invite you for an interview.
+    We are pleased to inform you that your application has been shortlisted. We would like to invite you for an interview.
 
-        Please reply to this email to confirm your availability for the next week.
+    Please reply to this email to confirm your availability for the next week.
 
-        Best regards,
-        {company_name} HR Team
+    Best regards,
+    {company_name} HR Team
     """
     
     try:
